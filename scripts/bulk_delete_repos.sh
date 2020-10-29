85d153dbd55ae4d6d6e20ee9be3e074879f82214
Dave Avis <charles.avis@springbranchisd.com>
2020-10-29 11:22:57 -0500

    Update bulk_delete_repos.sh

--- scripts/bulk_delete_repos.sh
+++ scripts/bulk_delete_repos.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Create an authorization token at GitHub.com and replace TOKEN with your token.
+# Create an authorization token at GitHub.com and put it in TOKEN.
 # Usage:
 # ./bulk_delete_repos.py filename.txt
 # filename.txt is a list of repo names to delete, one per line (example: StratfordHS-APCS/lab-00-hello-world-username)
 # Use get_repo_list_from_org.sh to create the list
 
+TOKEN=""
+
 while read i
 do 
 	repo=$(echo $i | xargs)
 	echo "Deleting $repo"
-	curl -XDELETE -H 'Authorization: token TOKEN' "https://api.github.com/repos/$repo"
+	curl -XDELETE -H 'Authorization: token $TOKEN' "https://api.github.com/repos/$repo"
 done < $1 
