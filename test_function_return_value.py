453e0026dcdf3d28c0eb7719c90f9449932baa5c
Dave Avis <charles.avis@springbranchisd.com>
2020-10-29 13:44:36 -0500

    Delete test_function_return_value.py

--- test_function_return_value.py
+++
@@ -1,6 +0,0 @@
-# This code goes in the file 'test_main.py'
-# This is an example of testing the return value from a function in main.py
-import main
-
-def test_30_105():
-    assert main.gcf(30,105) == 15, 'Incorrect result for gcf(30, 105)'
